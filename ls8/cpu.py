30dd52061264243c164310cbf4f419ca4874fda3
Chris Dobek <dobek.christopher@gmail.com>
2020-08-17 19:00:51 -0400

    Monday MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,9 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0         
 
     def load(self):
         """Load a program into memory."""
@@ -29,7 +30,12 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+    
+    def ram_read(self, mar):
+         return self.ram[mar]
 
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -61,5 +67,30 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        running = True
+
+        while running:
+            instruction = self.ram_read(self.pc)
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+            if instruction == HLT:
+                running = False
+                self.pc += 1
+                sys.exit()
+            elif instruction == MUL:
+                print(self.reg[reg_a] * self.reg[reg_b])
+                self.pc += 3
+            elif instruction == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+            elif instruction == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+            else:
+                print(f'this instruction is not valid: {hex(instruction)}')
+                running = False
+                sys.exit()
